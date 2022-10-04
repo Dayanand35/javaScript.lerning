521bd9742f97316fdae35d986418c2ede1553aac
Dayanand35 <dayanandvangwade@gmail.com>
2022-10-04 17:27:44 +0530

    demo.js
    demo.js

---
+++ demo.js
@@ -0,0 +1,12 @@
+
+// console.log("HelloW");
+// console.log("MY NAME IS DAYANAND");
+// console.info("THIS IS INFO"); // info mean info
+// console.warn("i am not that bad guy");// warn mean warning
+// console.error("opps failed to execute")//error
+console.log("DAYANAND GOVIND WANGVADE");
+console.log("reading,learning,photograpy,cooking");//my hobbies
+console.info("communication,problem solving,creativity,leadership");//this is my strength
+console.warn("self-criticism,procrastination,lack of experience,public speaking");//this is my warn
+console.log(30+50);//get addition of two number
+console.log(49*479);//get multiplecatin of two number 49*479
