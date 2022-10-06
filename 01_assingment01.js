cf4d78480a08d27bdc69c0ed3fc5b54b1aafed50
Dayanand35 <dayanandvangwade@gmail.com>
2022-10-06 14:47:21 +0530

    traile.js
    traile.js

---
+++ 01_assingment01.js
@@ -0,0 +1,16 @@
+
+var myNumber;
+myNumber=100;
+console.log(myNumber);
+
+var myLove;
+var myLove = "(i love only js)"// i love only js
+console.log(myLove);
+
+var temp;
+var temp = 200;
+console.log("before updeting value of temp",temp);
+temp = "java script";
+console.log("after updating of temp",temp);
+
+
---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+var my_myapple;
+my_apple = "mango";
+
+var my_name = "dayanand wangvade"; //variable decleration and initilization on same line
+my_name = "dayanand govind wangvade"
+console.log(my_name);
+console.log(my_apple);
+
+my_apple = "200";
+console.log(my_apple);
+
