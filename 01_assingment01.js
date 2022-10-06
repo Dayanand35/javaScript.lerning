031358ce20095822ab7fd95c48d383fe64598df5
Dayanand35 <dayanandvangwade@gmail.com>
2022-10-06 16:46:00 +0530

    assingment.js
    assingment.js

--- 01_assingment01.js
+++ 01_assingment01.js
@@ -13,4 +13,27 @@ console.log("before updeting value of temp",temp);
 temp = "java script";
 console.log("after updating of temp",temp);
 
+//Q2
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("befor swap value:1",num1,num2,num3);
+var temp = 200;
+num1 = temp;
+num2 = num3;
+num3 = num1;
+console.log("after swap value:1",num1,num2,num3)
 
+//Q3
+var name = "dayanand";
+console.log("my name is",name)
+var emai = "dayanandvangwade@gmail.com";
+console.log("my email id",emai)
+var age = 22;
+console.log("my age is",age);
+var addres = "wagholi ,pune";
+console.log("my address",addres);
+var gender = 'male';
+console.log("gender",gender)
+var married="i am unmarried";
+console.log("",married)
